2eae437d88df9bb2a5ca4b9566da46f30c6454b8
fancyqube <761530511@qq.com>
2018-09-11 11:02:50 +0800

    commit test

--- skuapp/table/t_amazon_orders_by_receive_day_total.py
+++ skuapp/table/t_amazon_orders_by_receive_day_total.py
@@ -13,15 +13,15 @@ from django.db import models
 
 
 class t_amazon_orders_by_receive_day_total(models.Model):
-    is_fba = models.IntegerField(u'是否FBA', max_length=1, blank=True, null=True)
-    product_sku = models.CharField(u'商品SKU', max_length=32, blank=True, null=True)
-    sku_unit_price = models.FloatField(u'商品单价', max_length=10, blank=True, null=True)
-    quantity = models.IntegerField(u'库存量', max_length=10, blank=True, null=True)
-    total_price = models.FloatField(u'总成本', max_length=12, blank=True, null=True)
-    UpdateTime = models.DateTimeField(u'更新时间', blank=True, null=True)
+    seller = models.CharField(u'销售员', max_length=32, blank=True, null=True)
+    site = models.CharField(u'站点', max_length=32, blank=True, null=True)
+    has_order = models.IntegerField(u'出单', max_length=10, blank=True, null=True)
+    no_order = models.IntegerField(u'出单', max_length=10, blank=True, null=True)
+    time_span = models.CharField(u'到货时间范围', max_length=32, blank=True, null=True)
+    refresh_time = models.DateTimeField(u'更新时间', blank=True, null=True)
 
     class Meta:
-        verbose_name = u'AMZ商品库存成本'
+        verbose_name = u'AMZ按到货日期出单情况'
         verbose_name_plural = verbose_name
         db_table = 't_amazon_orders_by_receive_day_total'
 
